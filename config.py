fa207fb4ce1254b07ba327c44dd73d4e160dee58
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-06-12 20:51:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,12 +27,12 @@ id_pattern = re.compile(r'^.\d+$')
 class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "23990433")
-    API_HASH  = os.environ.get("API_HASH", "")
+    API_HASH  = os.environ.get("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5992501587:AAHdDLpHnleGcpCQUizT1efHLhDNYUrtuYA") 
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","pyro-botz")     
-    DB_URL  = os.environ.get("DB_URL","e6c4b6ee1933711bc4da9d7d17e1eb20")
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://nakflixbot:alpha3720@cluster0.qgybxbu.mongodb.net/?retryWrites=true&w=majority")
  
     # other configs
     BOT_UPTIME  = time.time()
